6dd35303fb838b5b41ffb7c57ccd32e88553bf29
AmreshRoy <amoreshray@gmail.com>
2021-01-27 01:18:50 +0600

    add pricing table with repeatble field

---
+++ alpha/page-templates/pricingtable.php
@@ -0,0 +1,28 @@
+<?php
+/**
+*Template Name: Pricing Table
+ */
+get_header();
+$pricing = get_post_meta(get_the_ID(), "_alpha_pt_pricing_table", true);
+?>
+<div class="container">
+    <div class="row">
+    <?php
+    foreach ( $pricing as $ptc):
+        ?>
+        <div class="col-md-4">
+            <h2><?php echo esc_html($ptc['_alpha_pt_pricing_caption'])?></h2>
+            <ul>
+            <?php
+            foreach ($ptc['_alpha_pt_pricing_option'] as $pto):
+                ?>
+                <li><?php echo esc_html($pto); ?></li>
+            <?php endforeach; ?>
+            <h2><?php echo esc_html($ptc['_alpha_pt_price'])?></h2>
+            </ul>
+        </div>
+    <?php endforeach; ?>
+    </div>
+</div>
+<?php
+get_footer();
